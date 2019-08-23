00e99af9feca68a96fc1a2fbc5b2f0c0f75744d4
James <>
2019-08-23 13:42:12 +0200

    clean code

--- HeartRate.ino
+++ HeartRate.ino
@@ -1,42 +1,5 @@
 /*
-   Sketch to handle each sample read from a PulseSensor.
-   Typically used when you don't want to use interrupts
-   to read PulseSensor voltages.
-
-   Here is a link to the tutorial that discusses this code
-   https://pulsesensor.com/pages/getting-advanced
-
-   Copyright World Famous Electronics LLC - see LICENSE
-   Contributors:
-     Joel Murphy, https://pulsesensor.com
-     Yury Gitman, https://pulsesensor.com
-     Bradford Needham, @bneedhamia, https://bluepapertech.com
-
-   Licensed under the MIT License, a copy of which
-   should have been included with this software.
-
-   This software is not intended for medical use.
-*/
-
-/*
-   Every Sketch that uses the PulseSensor Playground must
-   define USE_ARDUINO_INTERRUPTS before including PulseSensorPlayground.h.
-   Here, #define USE_ARDUINO_INTERRUPTS false tells the library to
-   not use interrupts to read data from the PulseSensor.
-
-   If you want to use interrupts, simply change the line below
-   to read:
-     #define USE_ARDUINO_INTERRUPTS true
-
-   Set US_PS_INTERRUPTS to false if either
-   1) Your Arduino platform's interrupts aren't yet supported
-   by PulseSensor Playground, or
-   2) You don't wish to use interrupts because of the side effects.
-
-   NOTE: if US_PS_INTERRUPTS is false, your Sketch must
-   call pulse.sawNewSample() at least once every 2 milliseconds
-   to accurately read the PulseSensor signal.
-*/
+ */
 #define USE_ARDUINO_INTERRUPTS false
 #include <PulseSensorPlayground.h>
 
@@ -52,19 +15,7 @@
 */
 const int OUTPUT_TYPE = SERIAL_PLOTTER;
 
-/*
-   Pinout:
-     PULSE_INPUT = Analog Input. Connected to the pulse sensor
-      purple (signal) wire.
-     PULSE_BLINK = digital Output. Connected to an LED (and 220 ohm resistor)
-      that will flash on each detected pulse.
-     PULSE_FADE = digital Output. PWM pin onnected to an LED (and resistor)
-      that will smoothly fade with each pulse.
-      NOTE: PULSE_FADE must be a pin that supports PWM.
-       If USE_INTERRUPTS is true, Do not use pin 9 or 10 for PULSE_FADE,
-       because those pins' PWM interferes with the sample timer.
-*/
-const int PULSE_INPUT = A0;
+const int PULSE_INPUT = A0;  //PIN A0 is on Signal of Pulse sensor
 const int PULSE_BLINK = 13;    // Pin 13 is the on-board LED
 const int THRESHOLD = 516;   // A ajuster en fonction de votre montage
 /*
@@ -85,15 +36,15 @@ PulseSensorPlayground pulseSensor;
 
 
 /*LEDS
- 
  */
 int ledPins[] = {
   2, 3, 4, 5, 6, 7,8,9,10,11
 };       // an array of pin numbers to which LEDs are attached
 int pinCount = 10;  
 
-     const byte bpmMin = 40;
-     const byte bpmMax = 110;
+//Valeur minimal du rythme cardiaque a afficher sur les leds
+const byte bpmMin = 40;
+const byte bpmMax = 110;
 
 //SETUP
 void setup() {
@@ -107,8 +58,10 @@ void setup() {
   pulseSensor.setOutputType(OUTPUT_TYPE);
   pulseSensor.setThreshold(THRESHOLD);
 
-  // Skip the first SAMPLES_PER_SERIAL_SAMPLE in the loop().
-  samplesUntilReport = SAMPLES_PER_SERIAL_SAMPLE;
+ //LEDS SETUP in Outupu
+  for (int thisPin = 0; thisPin < pinCount; thisPin++) {
+    pinMode(ledPins[thisPin], OUTPUT);
+  }
 
   // Now that everything is ready, start reading the PulseSensor signal.
   if (!pulseSensor.begin()) {//FAILED
@@ -127,10 +80,7 @@ void setup() {
       delay(50);
     }
   }
-  //LEDS SETPE
-  for (int thisPin = 0; thisPin < pinCount; thisPin++) {
-    pinMode(ledPins[thisPin], OUTPUT);
-  }
+ 
 }
 
 void loop() {
@@ -165,26 +115,20 @@ void loop() {
         pulseSensor.outputBeat();
         int myBPM = pulseSensor.getBeatsPerMinute();
 
-         Serial.println("♥  A HeartBeat Happened ! "); // If test is "true", print a message "a heartbeat happened".
- Serial.print("BPM: ");                        // Print phrase "BPM: " 
- Serial.println(myBPM);  
+        Serial.println("♥  A HeartBeat Happened ! "); // If test is "true", print a message "a heartbeat happened".
+        Serial.print("BPM: ");                        // Print phrase "BPM: " 
+        Serial.println(myBPM);  
 
-    if ( myBPM >= bpmMin && myBPM <= bpmMax ) {
-     int purcent  = map(myBPM, bpmMin , bpmMax, 0, 100);
-     lightLed100(purcent);
-     delay(100);
-     lightLed100(0);
-     }else{
+          if ( myBPM >= bpmMin && myBPM <= bpmMax ) {
+           int purcent  = map(myBPM, bpmMin , bpmMax, 0, 100);
+           lightLed100(purcent);
+           delay(100);
            lightLed100(0);
-     }
-  
-      }
+           }else{
+                 lightLed100(0);
+           }
+       }
     }
-
-    /*******
-      Here is a good place to add code that could take up
-      to a millisecond or so to run.
-    *******/
   }
 
   /******
